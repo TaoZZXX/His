4b6ae2c696dcfe607a98ceb92c3e4d23f6c02353
TaoZZXX <792277389@qq.com>
2026-01-06 22:55:44 +0800

    25-1-6

--- his-web/src/main.js
+++ his-web/src/main.js
@@ -1,51 +1,18 @@
 import Vue from 'vue'
+import App from './App.vue'
 
-import Cookies from 'js-cookie'
+// 1. 引入 Element UI 核心库和样式
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
-import 'normalize.css/normalize.css' // a modern alternative to CSS resets
+// 2. 全局注册 Element UI
+Vue.use(ElementUI)
 
-import Element from 'element-ui'
-import './styles/element-variables.scss'
-
-import '@/styles/index.scss' // global css
-
-import App from './App'
-import store from './store'
 import router from './router'
-
-import './icons' // icon
-import './permission' // permission control
-import './utils/error-log' // error log
-
-import * as filters from './filters' // global filters
-
-/**
- * If you don't want to use mock-server
- * you want to use MockJs for mock api
- * you can execute: mockXHR()
- *
- * Currently MockJs will be used in the production environment,
- * please remove it before going online! ! !
- */
-import { mockXHR } from '../mock'
-if (process.env.NODE_ENV === 'production') {
-  mockXHR()
-}
-
-Vue.use(Element, {
-  size: Cookies.get('size') || 'medium' // set element-ui default size
-})
-
-// register global utility filters
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
 
 Vue.config.productionTip = false
 
 new Vue({
-  el: '#app',
   router,
-  store,
   render: h => h(App)
-})
+}).$mount('#app')
